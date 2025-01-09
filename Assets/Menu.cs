81bcb692284c50d26a9526b344845dcbc5fb9002
Achitphonn <achitphon.kaenphut@elev.ga.lbs.se>
2025-01-09 10:36:20 +0100

    Menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,18 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+        
+    
+}
