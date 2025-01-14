81a535a079eefeed47c675d703256ec289c80cb5
Marcuzs16 <marcus.nilssonwikman@elev.lbs.se>
2025-01-14 14:12:47 +0100

    Enemy detection

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Image warningImage;
+    private void Start()
+    {
+        if (warningImage != null)
+        {
+            warningImage.gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            warningImage.gameObject.SetActive(true);
+            Invoke("Dölj Varningstecken", 3f); //Detta gör så att varningstecknet går bort efter 3 sekunder som den varit uppe
+        }
+        void HideWarning()
+        {
+            warningImage.gameObject.SetActive(false);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
